4fb66b61a42f5322819c005cfe7fcb6dd446cc98
tomoshino <zm18214@josai.ac.jp>
2020-01-14 04:03:25 +0000

    1/14

---
+++ 1-2.js
@@ -0,0 +1,16 @@
+
+function sayhello1(){
+alert('趣味');
+}
+
+function sayhello2(){
+alert('好きなゲーム');
+}
+
+function sayhello3(){
+alert('よく聞く曲');
+}
+
+function sayhello4(){
+alert('中学、高校でやっていたこと');
+}
